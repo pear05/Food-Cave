1dd9fbf744cdcc47bd4a3fca7dbd2b7750a95ee1
Waswa Austin <70325153+pear05@users.noreply.github.com>
2024-03-27 22:40:53 +0300

    Create tailwind.jsx

---
+++ tailwind.jsx
@@ -0,0 +1,83 @@
+<div className="Home w-[390px] h-[844px] relative bg-neutral-900">
+  <div className="StatusBarIphone w-[459px] h-8 left-[-40px] top-[-8px] absolute bg-white justify-center items-start gap-[130.81px] inline-flex">
+    <div className="Time h-[54px] pl-[54.57px] pr-[70.65px] pt-[18.34px] pb-[13.66px] justify-start items-center inline-flex">
+      <div className="Time text-center text-black text-[17px] font-['SF Pro'] leading-snug">9:41</div>
+    </div>
+    <div className="Levels w-[164.10px] h-[54px] pl-[42px] pr-[43.77px] pt-[23px] pb-[18px] justify-center items-start gap-[7.50px] inline-flex">
+      <div className="Battery w-[27.33px] h-[13px] relative">
+        <div className="Border w-[25px] h-[13px] left-0 top-0 absolute opacity-30 rounded border border-black" />
+        <div className="Capacity w-[21px] h-[9px] left-[2px] top-[2px] absolute bg-black rounded-sm" />
+      </div>
+    </div>
+  </div>
+  <div className="StatusBarIphone w-[390px] h-[35px] left-0 top-[-1px] absolute bg-white justify-center items-start gap-[111.15px] inline-flex">
+    <div className="Time h-[54px] pl-[54.57px] pr-[45.98px] pt-[18.34px] pb-[13.66px] justify-end items-center inline-flex">
+      <div className="Time text-center text-black text-[17px] font-['SF Pro'] leading-snug">9:41</div>
+    </div>
+    <div className="Levels w-[139.43px] h-[54px] pl-[30px] pr-[31.10px] pt-[23px] pb-[18px] justify-center items-start gap-[7.50px] inline-flex">
+      <div className="Battery w-[27.33px] h-[13px] relative">
+        <div className="Border w-[25px] h-[13px] left-0 top-0 absolute opacity-30 rounded border border-black" />
+        <div className="Capacity w-[21px] h-[9px] left-[2px] top-[2px] absolute bg-black rounded-sm" />
+      </div>
+    </div>
+  </div>
+  <div className="Group1 w-14 h-14 left-[324px] top-[49px] absolute">
+    <div className="Rectangle4 w-14 h-14 left-0 top-0 absolute bg-amber-200 rounded-[15px]" />
+    <div className="Profile w-[41px] h-[35px] pl-[6.94px] pr-[7px] pt-[3.06px] pb-[2.96px] left-[8px] top-[10px] absolute justify-center items-center inline-flex">
+      <div className="Profile w-[27.06px] relative">
+      </div>
+    </div>
+  </div>
+  <div className="GoodMorningLetsCook left-[9px] top-[49px] absolute text-orange-200 text-[25px] font-bold font-['Poppins']">Good Morning ,   Lets cook.</div>
+  <div className="SearchForRecipies w-[355px] h-[50px] left-[15px] top-[142px] absolute">
+    <div className="Rectangle5 w-[355px] h-11 left-0 top-[3px] absolute bg-zinc-300 rounded-[50px]" />
+    <div className="SearchForRecipies w-[188px] left-[61px] top-[17px] absolute text-stone-500 text-base font-normal font-['Inter']">Search For Recipies</div>
+    <div className="Search w-6 h-6 pl-0.5 pr-[1.71px] pt-0.5 pb-[1.25px] left-[19px] top-[13px] absolute justify-center items-center inline-flex">
+      <div className="Search w-[20.26px] h-[20.72px] relative">
+      </div>
+    </div>
+    <img className="Tune w-[25px] h-[50px] left-[320px] top-0 absolute" src="https://via.placeholder.com/25x50" />
+  </div>
+  <div className="Rectangle9 w-[50px] h-[50px] left-[16px] top-[248px] absolute bg-zinc-300 rounded-[10px]" />
+  <div className="Rectangle10 w-[50px] h-[50px] left-[92px] top-[248px] absolute bg-zinc-300 rounded-[10px]" />
+  <div className="Rectangle11 w-[50px] h-[50px] left-[168px] top-[248px] absolute bg-zinc-300 rounded-[10px]" />
+  <div className="Rectangle12 w-[50px] h-[50px] left-[244px] top-[248px] absolute bg-zinc-300 rounded-[10px]" />
+  <div className="Rectangle13 w-[50px] h-[50px] left-[320px] top-[248px] absolute bg-zinc-300 rounded-[10px]" />
+  <img className="Beef w-6 h-[90px] left-[26px] top-[228px] absolute" src="https://via.placeholder.com/24x90" />
+  <img className="Milkshake w-[25px] h-[90px] left-[104px] top-[228px] absolute" src="https://via.placeholder.com/25x90" />
+  <img className="IcePop w-[25px] h-[90px] left-[332px] top-[228px] absolute" src="https://via.placeholder.com/25x90" />
+  <div className="PageControl w-[390px] h-[30px] py-[3px] left-[-2px] top-[328px] absolute justify-center items-center inline-flex">
+    <div className="Frame px-3 py-2 bg-stone-950 rounded-[50px] backdrop-blur-2xl justify-center items-center gap-2 inline-flex">
+      <div className="Dot1 w-2 h-2 relative bg-black rounded-[50px]" />
+      <div className="Dot2 w-2 h-2 relative opacity-30 bg-black rounded-[50px]" />
+    </div>
+  </div>
+  <div className="Meats left-[16px] top-[301px] absolute text-center text-white text-[17px] font-normal font-['Inter'] leading-snug">Meats</div>
+  <div className="Drinks left-[92px] top-[301px] absolute text-center text-white text-[17px] font-normal font-['Inter'] leading-snug">Drinks</div>
+  <div className="Vegan left-[169px] top-[301px] absolute text-center text-white text-[17px] font-normal font-['Inter'] leading-snug">Vegan</div>
+  <div className="Dessert left-[314px] top-[301px] absolute text-center text-white text-[17px] font-normal font-['Inter'] leading-snug">Dessert</div>
+  <img className="Croissant w-[25px] h-[90px] left-[255px] top-[228px] absolute" src="https://via.placeholder.com/25x90" />
+  <div className="Bake left-[247px] top-[301px] absolute text-center text-white text-[17px] font-normal font-['Inter'] leading-snug">Bake</div>
+  <img className="Broccoli w-[25px] h-[90px] left-[178px] top-[228px] absolute" src="https://via.placeholder.com/25x90" />
+  <div className="PopularWeeklyRecipies w-[335px] h-[15px] left-[8px] top-[383px] absolute text-white text-[26px] font-semibold font-['Inter'] leading-snug">Popular Weekly Recipies</div>
+  <div className="Rectangle15 w-[190px] h-[150px] left-[190px] top-[428px] absolute rounded-[20px]" />
+  <div className="Group6 h-[194px] left-[26px] top-[623px] absolute">
+    <img className="Rectangle16 w-[150px] h-[150px] left-0 top-0 absolute rounded-[20px]" src="https://via.placeholder.com/150x150" />
+    <div className="DeepFriedChicken190Cal w-[145px] h-[37px] left-[2px] top-[157px] absolute text-white text-xs font-normal font-['Inter']">Deep Fried Chicken           190 Cal</div>
+  </div>
+  <div className="Group5 h-48 left-[207px] top-[428px] absolute">
+    <img className="Rectangle17 w-[150px] h-[150px] left-0 top-0 absolute rounded-[20px]" src="https://via.placeholder.com/150x150" />
+    <div className="MeatLoverSPizza250Cal w-[145px] h-[37px] left-[5px] top-[155px] absolute text-white text-xs font-normal font-['Inter']">Meat Lover's Pizza          250 Cal</div>
+  </div>
+  <div className="Group7 h-48 left-[207px] top-[625px] absolute">
+    <img className="Rectangle17 w-[150px] h-[150px] left-0 top-0 absolute rounded-[20px]" src="https://via.placeholder.com/150x150" />
+    <div className="RoastedBeefSteak300Cal w-[145px] h-[37px] left-[5px] top-[155px] absolute text-white text-xs font-normal font-['Inter']">Roasted Beef Steak     300 Cal</div>
+  </div>
+  <div className="Group3 h-48 left-[26px] top-[428px] absolute">
+    <img className="Rectangle16 w-[150px] h-[150px] left-0 top-0 absolute rounded-[20px]" src="https://via.placeholder.com/150x150" />
+    <div className="BurgerWedgesCombo150Cal w-[145px] h-[37px] left-0 top-[155px] absolute text-white text-xs font-normal font-['Inter']">Burger & Wedges Combo           150 Cal</div>
+  </div>
+  <div className="HomeIndicator w-[392px] h-[21px] pl-[126px] pr-[127px] left-[-2px] top-[823px] absolute justify-center items-center inline-flex">
+    <div className="HomeIndicator w-[139px] h-[5px] origin-top-left rotate-180 bg-white rounded-[100px]" />
+  </div>
+</div>
